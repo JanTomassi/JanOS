a423b5cbee8f35ac72d94fd9174d4562bec6b8fc
Jan Tomassi <tomassijan@gmail.com>
2024-06-03 16:02:43 +0200

    feat: div printable and not printable key code

--- kernel/include/kernel/key_code.h
+++ kernel/include/kernel/key_code.h
@@ -1,8 +1,8 @@
 #pragma once
 
 enum key_code {
-	KEY_CODE_0, // 0 (zero) pressed
-	KEY_CODE_1, // 1_p
+	KEY_CODE_0, // 0 pressed
+	KEY_CODE_1, // 1 pressed
 	KEY_CODE_2, // 2 pressed
 	KEY_CODE_3, // 3 pressed
 	KEY_CODE_4, // 4 pressed
@@ -12,32 +12,78 @@ enum key_code {
 	KEY_CODE_8, // 8 pressed
 	KEY_CODE_9, // 9 pressed
 	KEY_CODE_A, // A pressed
-	KEY_CODE_ACPI_POWER, // (ACPI) power pressed
-	KEY_CODE_ACPI_SLEEP, // (ACPI) sleep pressed
-	KEY_CODE_ACPI_WAKE, // (ACPI) wake pressed
-	KEY_CODE_APPS, // "apps" pressed
+	KEY_CODE_B, // B pressed
+	KEY_CODE_C, // C pressed
+	KEY_CODE_D, // D pressed
+	KEY_CODE_E, // E pressed
+	KEY_CODE_F, // F pressed
+	KEY_CODE_G, // G pressed
+	KEY_CODE_H, // H pressed
+	KEY_CODE_I, // I pressed
+	KEY_CODE_J, // J pressed
+	KEY_CODE_K, // K pressed
+	KEY_CODE_L, // L pressed
+	KEY_CODE_M, // M pressed
+	KEY_CODE_N, // N pressed
+	KEY_CODE_O, // O pressed
+	KEY_CODE_P, // P pressed
+	KEY_CODE_Q, // Q pressed
+	KEY_CODE_R, // R pressed
+	KEY_CODE_S, // S pressed
+	KEY_CODE_T, // T pressed
+	KEY_CODE_U, // U pressed
+	KEY_CODE_V, // V pressed
+	KEY_CODE_W, // W pressed
+	KEY_CODE_X, // X pressed
+	KEY_CODE_Y, // Y pressed
+	KEY_CODE_Z, // Z pressed
+
+	KEY_CODE_BACKTICK, // ` (back tick) pressed
+	KEY_CODE_MINUS, // - pressed
+	KEY_CODE_EQ, // = pressed
+	KEY_CODE_LSBRACKET, // [ pressed
+	KEY_CODE_RSBRACKET, // ] pressed
+	KEY_CODE_BACKSLASH, // \ pressed
+	KEY_CODE_SEMICOLON, // ; pressed
+	KEY_CODE_SQUOTE, // ' (single quote) pressed
+	KEY_CODE_COMMA, // , pressed
+	KEY_CODE_POINT, // . pressed
+	KEY_CODE_SLASH, // / pressed
+
+	KEY_CODE_LAST_PRINTABLE, // marker for the last printable char
+
+	KEY_CODE_SPACE, // space pressed
+	KEY_CODE_TAB, // tab pressed
+
+	KEY_CODE_ENTER, // enter pressed
+	KEY_CODE_BACKSPACE, // backspace pressed
+	KEY_CODE_CAPSLOCK, // CapsLock pressed
+
+	KEY_CODE_ESC, // ESC pressed
+
+	KEY_CODE_HOME, // home pressed
+	KEY_CODE_PAGE_UP, // page up pressed
+	KEY_CODE_PAGE_DOWN, // page down pressed
+	KEY_CODE_END, // end pressed
+
+	KEY_CODE_INSERT, // insert pressed
+	KEY_CODE_DELETE, // delete pressed
+
+	KEY_CODE_ARROW_UP, // cursor up pressed
 	KEY_CODE_ARROW_DOWN, // cursor down pressed
 	KEY_CODE_ARROW_LEFT, // cursor left pressed
-	KEY_CODE_ARROW_UP, // cursor up pressed
-	KEY_CODE_B, // B pressed
-	KEY_CODE_BACKSLASH, // \ pressed
-	KEY_CODE_BACKSPACE, // backspace pressed
-	KEY_CODE_BACKTICK, // ` (back tick) press
-	KEY_CODE_C, // C pressed
-	KEY_CODE_CAPSLOCK, // CapsLock pressed
-	KEY_CODE_COMMA, // , pressed
-	KEY_CODE_CURSOR_RIGHT, // cursor right pressed
-	KEY_CODE_D, // D pressed
-	KEY_CODE_DELETE, // delete pressed
-	KEY_CODE_E, // E pressed
-	KEY_CODE_END, // end pressed
-	KEY_CODE_ENTER, // enter pressed
-	KEY_CODE_EQ, // = pressed
-	KEY_CODE_F, // F pressed
+	KEY_CODE_ARROW_RIGHT, // cursor right pressed
+
+	KEY_CODE_LALT, // left alt pressed
+	KEY_CODE_RALT, // right alt (or altGr) pressed
+	KEY_CODE_LCTRL, // left control presse
+	KEY_CODE_RCTRL, // right control pressed
+	KEY_CODE_LGUI, // left GUI pressed
+	KEY_CODE_RGUI, // right GUI pressed
+	KEY_CODE_LSHIFT, // left shift pressed
+	KEY_CODE_RSHIFT, // right shift pressed
+
 	KEY_CODE_F1, // F1 pressed
-	KEY_CODE_F10, // F10 pressed
-	KEY_CODE_F11, // F11 pressed
-	KEY_CODE_F12, // F12 pressed
 	KEY_CODE_F2, // F2 pressed
 	KEY_CODE_F3, // F3 pressed
 	KEY_CODE_F4, // F4 pressed
@@ -46,13 +92,11 @@ enum key_code {
 	KEY_CODE_F7, // F7 pressed
 	KEY_CODE_F8, // F8 pressed
 	KEY_CODE_F9, // F9 pressed
-	KEY_CODE_G, // G pressed
-	KEY_CODE_H, // H pressed
-	KEY_CODE_HOME, // home pressed
-	KEY_CODE_I, // I pressed
-	KEY_CODE_INSERT, // insert pressed
-	KEY_CODE_J, // J pressed
-	KEY_CODE_K, // K pressed
+	KEY_CODE_F10, // F10 pressed
+	KEY_CODE_F11, // F11 pressed
+	KEY_CODE_F12, // F12 pressed
+
+	KEY_CODE_NUMBERLOCK, // NUMBERLOCK PRESSED
 	KEY_CODE_KEYPAD_0, // (keypad) 0 pressed
 	KEY_CODE_KEYPAD_1, // (keypad) 1 pressed
 	KEY_CODE_KEYPAD_2, // (keypad) 2 pressed
@@ -69,14 +113,16 @@ enum key_code {
 	KEY_CODE_KEYPAD_POINT, // (keypad) . pressed
 	KEY_CODE_KEYPAD_SLASH, // (keypad) / pressed
 	KEY_CODE_KEYPAD_STAR, // (keypad) * pressed
-	KEY_CODE_L, // L pressed
-	KEY_CODE_LALT, // left alt pressed
-	KEY_CODE_LCTRL, // left control presse
-	KEY_CODE_LGUI, // left GUI pressed
-	KEY_CODE_LSBRACKET, // [ pressed
-	KEY_CODE_LSHIFT, // left shift pressed
-	KEY_CODE_M, // M pressed
-	KEY_CODE_MINUS, // - pressed
+
+	KEY_CODE_PAUSE, // paused pressed
+	KEY_CODE_PRINTSCREEN, // screen pressed
+	KEY_CODE_SCROLLLOCK, // ScrollLock pressed
+
+	KEY_CODE_ACPI_POWER, // (ACPI) power pressed
+	KEY_CODE_ACPI_SLEEP, // (ACPI) sleep pressed
+	KEY_CODE_ACPI_WAKE, // (ACPI) wake pressed
+	KEY_CODE_APPS, // "apps" pressed
+
 	KEY_CODE_MULTIMEDIA_CALCULATOR, // (multimedia) calculator pressed
 	KEY_CODE_MULTIMEDIA_EMAIL, // (multimedia) email pressed
 	KEY_CODE_MULTIMEDIA_MEDIASELECT, // (multimedia) media select pressed
@@ -95,38 +141,8 @@ enum key_code {
 	KEY_CODE_MULTIMEDIA_WWW_REFRESH, // (multimedia) WWW refresh pressed
 	KEY_CODE_MULTIMEDIA_WWW_SEARCH, // (multimedia) WWW search pressed
 	KEY_CODE_MULTIMEDIA_WWW_STOP, // (multimedia) WWW stop pressed
-	KEY_CODE_N, // N pressed
-	KEY_CODE_NUMBERLOCK, // NUMBERLOCK PRESSED
-	KEY_CODE_O, // O pressed
-	KEY_CODE_P, // P pressed
-	KEY_CODE_PAGE_DOWN, // page down pressed
-	KEY_CODE_PAGE_UP, // page up pressed
-	KEY_CODE_PAUSE, // paused pressed
-	KEY_CODE_POINT, // . pressed
-	KEY_CODE_PRINTSCREEN, // screen pressed
-	KEY_CODE_Q, // Q pressed
-	KEY_CODE_R, // R pressed
-	KEY_CODE_RALT, // right alt (or altGr) pressed
-	KEY_CODE_RCTRL, // right control pressed
-	KEY_CODE_RGUI, // right GUI pressed
-	KEY_CODE_RSBRACKET, // ] pressed
-	KEY_CODE_RSHIFT, // right shift pressed
-	KEY_CODE_S, // S pressed
-	KEY_CODE_SCROLLLOCK, // ScrollLock pressed
-	KEY_CODE_SEMICOLON, // ; pressed
-	KEY_CODE_SLASH, // / pressed
-	KEY_CODE_SPACE, // space pressed
-	KEY_CODE_SQUOTE, // ' (single quote) pr
-	KEY_CODE_T, // T pressed
-	KEY_CODE_TAB, // tab pressed
-	KEY_CODE_U, // U pressed
-	KEY_CODE_V, // V pressed
-	KEY_CODE_W, // W pressed
-	KEY_CODE_X, // X pressed
-	KEY_CODE_Y, // Y pressed
-	KEY_CODE_Z, // Z pressed
-	KEY_CODE_ESC, // ESC pressed
-	KEY_CODE_NONE = 0, // undefined
+
+	KEY_CODE_NONE,
 };
 
 const char *KEY_CODE_str[] = {
