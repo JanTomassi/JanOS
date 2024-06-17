aa6c3cadec1c7622338ca15a814f6e2309d5f182
Jan Tomassi <tomassijan@gmail.com>
2024-06-17 14:35:04 +0200

    refactor: rm dead code

--- kernel/include/kernel/serial.h
+++ kernel/include/kernel/serial.h
@@ -4,7 +4,3 @@
 #include <stdint.h>
 
 display_t init_serial();
-/* void serial_write_str(char *); */
-/* void serial_write_unumber(uintmax_t num); */
-/* void serial_write_number(intmax_t num); */
-/* void serial_write_hex(uintmax_t num); */
