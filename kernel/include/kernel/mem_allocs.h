33ed98fce841ce9d99d9b6234e49f656ae6158b8
Jan Tomassi <tomassijan@gmail.com>
2024-06-18 14:20:29 +0200

    refactoring: adding mem name space to public fn

--- kernel/include/kernel/mem_allocs.h
+++ kernel/include/kernel/mem_allocs.h
@@ -31,22 +31,22 @@ void mem_register_tag(mem_malloc_tag_t *);
 void mem_unregister_tag(mem_malloc_tag_t *);
 
 // Insert phy_mem_tag sorted by ptr in the chain
-void insert_phy_mem_tag(mem_phy_mem_tag_t *tag, struct list_head *chain);
+void mem_insert_phy_mem_tag(mem_phy_mem_tag_t *tag, struct list_head *chain);
 // Give the phy_mem_tag back to free_phy_tags_list
-void give_phy_mem_tag(mem_phy_mem_tag_t *);
+void mem_give_phy_mem_tag(mem_phy_mem_tag_t *);
 // Get the phy_mem_tag from free_phy_tags_list
-mem_phy_mem_tag_t *get_phy_mem_tag();
+mem_phy_mem_tag_t *mem_get_phy_mem_tag();
 
 // Give phy_mem_link* back to free_phy_links_list
-void give_phy_mem_link(mem_phy_mem_link_t *);
+void mem_give_phy_mem_link(mem_phy_mem_link_t *);
 // Get phy_mem_link* from free_phy_links_list
-mem_phy_mem_link_t *get_phy_mem_link();
+mem_phy_mem_link_t *mem_get_phy_mem_link();
 // Get the phy mem tag from a list of phy_mem_link
-mem_phy_mem_tag_t *get_phy_tag_from_link(struct list_head *link);
+mem_phy_mem_tag_t *mem_get_phy_tag_from_link(struct list_head *link);
 
 // Insert malloc_tag sorted by ptr in the chain
-void insert_tag(mem_malloc_tag_t *tag, struct list_head *list);
+void mem_insert_tag(mem_malloc_tag_t *tag, struct list_head *list);
 // Give malloc_tag* back to free_tags_list
-void give_tag(mem_malloc_tag_t *);
+void mem_give_tag(mem_malloc_tag_t *);
 // Get malloc_tag* from free_tags_list
-mem_malloc_tag_t *get_tag();
+mem_malloc_tag_t *mem_get_tag();
