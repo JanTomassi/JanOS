6970a9624af4fccaf73e7a5be1a5325d4fdc86b0
Jan Tomassi <tomassijan@gmail.com>
2024-06-18 14:11:37 +0200

    feat: adding get/set for mem tag and phy mem tag

--- kernel/include/kernel/mem_allocs.h
+++ kernel/include/kernel/mem_allocs.h
@@ -8,6 +8,19 @@ typedef struct mem_malloc_tag mem_malloc_tag_t;
 typedef struct mem_phy_mem_link mem_phy_mem_link_t;
 typedef struct mem_phy_mem_tag mem_phy_mem_tag_t;
 
+fatptr_t mem_get_fatptr_phy_mem(const mem_phy_mem_tag_t *);
+size_t mem_get_refcnt_phy_mem(const mem_phy_mem_tag_t *);
+fatptr_t mem_set_fatptr_phy_mem(mem_phy_mem_tag_t *, fatptr_t val);
+size_t mem_set_refcnt_phy_mem(mem_phy_mem_tag_t *, size_t val);
+
+void *mem_get_ptr_tag(const mem_malloc_tag_t *);
+size_t mem_get_size_tag(const mem_malloc_tag_t *);
+size_t mem_get_used_tag(const mem_malloc_tag_t *);
+struct list_head *mem_get_chain_tag(const mem_malloc_tag_t *);
+void *mem_set_ptr_tag(mem_malloc_tag_t *, void *);
+size_t mem_set_size_tag(mem_malloc_tag_t *, size_t);
+size_t mem_set_used_tag(mem_malloc_tag_t *, size_t);
+
 // Insert phy_mem_tag sorted by ptr in the chain
 void insert_phy_mem_tag(mem_phy_mem_tag_t *tag, struct list_head *chain);
 // Give the phy_mem_tag back to free_phy_tags_list
