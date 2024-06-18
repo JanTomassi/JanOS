311198adb5833b43cdd36c38f5457d31191a0602
Jan Tomassi <tomassijan@gmail.com>
2024-06-18 14:45:47 +0200

    feat: adding def of General Purpose Allocator

---
+++ kernel/kernel/allocator.c
@@ -0,0 +1,13 @@
+#include <kernel/allocator.h>
+#include <kernel/mem_allocs.h>
+#include <kernel/display.h>
+
+fatptr_t mem_gpa_alloc(void)
+{
+	panic("TODO");
+}
+
+void mem_gpa_free(fatptr_t freeing)
+{
+	panic("TODO");
+}
