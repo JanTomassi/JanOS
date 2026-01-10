aafca87be514394a43671908c0fa049b5c7e1c20
Jan Tomassi <tomassijan@gmail.com>
2026-01-11 00:01:25 +0100

    feat: read fat entry

--- kernel/include/kernel/fat16.h
+++ kernel/include/kernel/fat16.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <kernel/storage.h>
 
@@ -41,7 +42,10 @@ typedef struct fat16_layout{
 	uint32_t root_dir_sectors;
 	uint32_t data_start_lba;
 	uint32_t sectors_per_cluster;
+	uint16_t sector_size;
 } fat16_layout_t;
 
 fat_BS_t *read_fat_boot_section(struct storage_device dev);
 void fat16_compute_layout(const fat_BS_t *bpb, fat16_layout_t *out);
+uint16_t fat16_read_fat_entry(const struct storage_device *device, const fat16_layout_t *layout, uint16_t cluster);
+bool fat16_is_end_of_chain(uint16_t entry);
