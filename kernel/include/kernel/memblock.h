5be24b7840cbc55ea016d558512205f8da6b02c1
Jan Tomassi <tomassijan@gmail.com>
2026-01-14 13:41:28 +0100

    feat: improve memblock alloc and init using mbi

--- kernel/include/kernel/memblock.h
+++ kernel/include/kernel/memblock.h
@@ -29,43 +29,9 @@ struct memblock {
 	bool bottom_up;
 };
 
-static inline size_t memblock_align_up(size_t addr, size_t align)
-{
-	if (align == 0)
-		return addr;
-
-	size_t mask = align - 1;
-	return (addr + mask) & ~mask;
-}
-
-static inline size_t memblock_align_down(size_t addr, size_t align)
-{
-	if (align == 0)
-		return addr;
-
-	size_t mask = align - 1;
-	return addr & ~mask;
-}
-
-static inline bool memblock_overlaps(size_t base1, size_t size1, size_t base2, size_t size2)
-{
-	if (size1 == 0 || size2 == 0)
-		return false;
-
-	size_t end1 = base1 + size1;
-	size_t end2 = base2 + size2;
-
-	if (end1 < base1)
-		end1 = SIZE_MAX;
-	if (end2 < base2)
-		end2 = SIZE_MAX;
-
-	return (base1 < end2) && (base2 < end1);
-}
-
-void memblock_init(bool bottom_up);
-void memblock_add(size_t base, size_t size);
-void memblock_reserve( size_t base, size_t size);
-void memblock_remove(size_t base, size_t size);
-size_t memblock_alloc_range(size_t size, size_t align, size_t start, size_t end);
-void memblock_free(size_t base, size_t size);
+void memblock_init(unsigned long mbi_addr, bool bottom_up);
+void memblock_add(struct memblock *block, size_t base, size_t size);
+void memblock_reserve(struct memblock *block, size_t base, size_t size);
+void memblock_remove(struct memblock *block, size_t base, size_t size);
+size_t memblock_alloc_range(struct memblock *block, size_t size, size_t align, size_t start, size_t end);
+void memblock_free(struct memblock *block, size_t base, size_t size);
