e0b011ba78aa7f97115411f670c6ec07d8aacecc
Jan Tomassi <tomassijan@gmail.com>
2026-09-04 14:25:19 +0200

    refactor: inject kernel boot tests

--- tests/kernel/test_entry.c
+++ tests/kernel/test_entry.c
@@ -1,6 +1,7 @@
 #include <kernel/init.h>
+#include <kernel/test.h>
 
 void kernel_main(unsigned int magic, unsigned long mbi_addr)
 {
-	kernel_initialize(magic, mbi_addr, nullptr);
+	kernel_initialize(magic, mbi_addr, kernel_test_boot);
 }
